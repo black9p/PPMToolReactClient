58494e0c90a84e83e81ef9f3f56bac31d2a39d74
black9p <black9p@naver.com>
2018-12-02 16:10:36 +0900

    Add Markup and Styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "./App.css";
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
