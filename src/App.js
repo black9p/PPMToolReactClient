1c23a86eafc369b76a5723e97f2d89159b761a69
black9p <black9p@naver.com>
2018-12-02 13:54:41 +0900

    Added ProjectItem and Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
 
 class App extends Component {
   render() {
     return ( 
       <div className = "App">
+        <Header />
         <Dashboard / >
       </div>
     );
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import ProjectItem from './Project/ProjectItem';
 
-export default class Dashboard extends Component {
+class Dashboard extends Component {
     render() {
         return ( 
             <div>
-                <h1> Welcome to the Dashboard </h1> 
+                <h1> Welcome to the Dashboard </h1>
+                <ProjectItem />
             </div>
         );
     }
 }
+
+export default Dashboard;
